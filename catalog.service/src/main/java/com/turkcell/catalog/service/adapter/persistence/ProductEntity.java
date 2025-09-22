efbdf9088af2927abf1d3dd92731e28b6edf3505
halitklyc <halitenes.kalayci@gmail.com>
2025-09-22 14:37:30 +0300

    add persistence adapters for ProductEntity

---
+++ catalog.service/src/main/java/com/turkcell/catalog/service/adapter/persistence/ProductEntity.java
@@ -0,0 +1,64 @@
+package com.turkcell.catalog.service.adapter.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name="products")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductEntity
+{
+    // Primitive/Supported Type -> UUID,BigDecimal,String
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String currency;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
---
+++ catalog.service/src/main/java/com/turkcell/catalog/service/adapter/persistence/ProductRepositoryAdapter.java
@@ -0,0 +1,38 @@
+package com.turkcell.catalog.service.adapter.persistence;
+
+import com.turkcell.catalog.service.application.port.out.ProductRepository;
+import com.turkcell.catalog.service.domain.Money;
+import com.turkcell.catalog.service.domain.Product;
+import com.turkcell.catalog.service.domain.ProductId;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepositoryAdapter  implements ProductRepository
+{
+    private final SpringDataProductRepository springDataProductRepository;
+
+    public ProductRepositoryAdapter(SpringDataProductRepository springDataProductRepository) {
+        this.springDataProductRepository = springDataProductRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setPrice(product.price().amount());
+        productEntity.setCurrency(product.price().currency());
+        productEntity.setDescription(product.description());
+        productEntity.setName(product.name());
+
+        productEntity = springDataProductRepository.save(productEntity);
+
+        Product productResponse = new Product(
+                new ProductId(productEntity.getId()),
+                productEntity.getName(),
+                productEntity.getDescription(),
+                new Money(productEntity.getPrice(), productEntity.getCurrency()),
+                null,
+                null
+        );
+        return productResponse;
+    }
+}
