5e78494bb642cf762bae01a1eaddf8e055de04a5
halitklyc <halitenes.kalayci@gmail.com>
2025-09-22 13:45:12 +0300

    create application layer

---
+++ catalog.service/src/main/java/com/turkcell/catalog/service/application/port/in/ProductUseCase.java
@@ -0,0 +1,5 @@
+package com.turkcell.catalog.service.application.port.in;
+
+// Ben bu işleri yapabilme kabiliyetine sahibim.
+// Dış dünyaya : "Ben bu hizmetleri sunuyorum"
+public interface ProductUseCase { }
---
+++ catalog.service/src/main/java/com/turkcell/catalog/service/application/port/out/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.turkcell.catalog.service.application.port.out;
+
+// Hizmetleri sunabilmem için aşağıdaki kuralları uygulayan bir sarmallayıcı vermelisin.
+public interface ProductRepository { }
+
+// 14:00
