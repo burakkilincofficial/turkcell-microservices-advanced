56e49aecef56c11daa76ead50fcb3750940822a3
halitklyc <halitenes.kalayci@gmail.com>
2025-09-23 13:22:13 +0300

    rest template ile senkron iletişim

---
+++ order.service/src/main/java/com/turkcell/orderservice/contract/GetProductByIdResponse.java
@@ -0,0 +1,61 @@
+package com.turkcell.orderservice.contract;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class GetProductByIdResponse {
+    private UUID id;
+    private String name;
+    private String description;
+    private int stock;
+    private BigDecimal price;
+    private String currency;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
---
+++ order.service/src/main/java/com/turkcell/orderservice/controller/OrdersController.java
@@ -0,0 +1,36 @@
+package com.turkcell.orderservice.controller;
+
+import com.turkcell.orderservice.contract.GetProductByIdResponse;
+import com.turkcell.orderservice.dto.CreateOrderDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrdersController {
+
+    private final RestTemplate restTemplate;
+
+    public OrdersController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @PostMapping
+    public String addOrder(@RequestBody List<CreateOrderDto> orders) {
+        //....CatalogService'e git her bir ürün için istenen miktarın stokda olup olmadığını kontrol et.
+        for (CreateOrderDto order : orders) {
+            var response = restTemplate
+                    .getForEntity("http://localhost:8888/api/v1/products/"+order.getProductId(), GetProductByIdResponse.class);
+            if (response.getStatusCode() != HttpStatus.OK  || order.getQuantity() > response.getBody().getStock())
+                throw new RuntimeException("Sorry, you don't have enough stock");
+        }
+        return "Sipariş Başarılı";
+    }
+}
