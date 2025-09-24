8aeff6443883c3e4f8d0977789b24cf70994159b
halitklyc <halitenes.kalayci@gmail.com>
2025-09-24 16:40:48 +0300

    Add redis & lettuce for rate limiter with bucket4j

--- catalog.service/src/main/java/com/turkcell/catalog/service/config/RateLimitFilter.java
+++ catalog.service/src/main/java/com/turkcell/catalog/service/config/RateLimitFilter.java
@@ -2,11 +2,20 @@ package com.turkcell.catalog.service.config;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.Refill;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,16 +24,34 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Component
 public class RateLimitFilter extends OncePerRequestFilter {
+    // INMemory
     private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
+    @Value("${spring.data.redis.uri:redis://localhost:6379}")
+    private String redisUri;
 
-    private Bucket newBucket() {
+    private LettuceBasedProxyManager<String> proxyManager;
+
+    @PostConstruct
+    void init() {
+        RedisClient redisClient = RedisClient.create(redisUri);
+        StatefulRedisConnection<String, byte[]> conn = redisClient
+                .connect(RedisCodec.of(new StringCodec(), new ByteArrayCodec()));
+        proxyManager = LettuceBasedProxyManager
+                .builderFor(conn, key -> ("bucket4j:"+key).getBytes())
+                .build();
+        System.out.println("Redis bağlantısı başarılı.");
+    }
+
+    private BucketConfiguration newBucket() {
         Bandwidth limitPerMinute = Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(1)));
         Bandwidth burst = Bandwidth.classic(20, Refill.intervally(20, Duration.ofMinutes(1)));
-        return Bucket.builder().addLimit(limitPerMinute).addLimit(burst).build();
+        return BucketConfiguration.builder().addLimit(limitPerMinute).addLimit(burst).build();
     }
 
     @Override
@@ -37,7 +64,11 @@ public class RateLimitFilter extends OncePerRequestFilter {
             ip = request.getRemoteAddr();
         }
 
-        Bucket bucket = buckets.computeIfAbsent(ip, k -> newBucket());
+        Bucket bucket = proxyManager
+                .builder()
+                .build(ip, (Function<String,BucketConfiguration>) k -> newBucket());
+
+        //buckets.computeIfAbsent(ip, k -> newBucket());
 
         if(bucket.tryConsume(1))
         {
